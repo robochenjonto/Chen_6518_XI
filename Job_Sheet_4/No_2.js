6182b4f6e98869afea2a9ffdb4f76d3bc79d8eb0
robochenjonto <chenhi5678@gmail.com>
2023-08-08 17:31:33 +0700

    ./

---
+++ Job_Sheet_4/No_2.js
@@ -0,0 +1,17 @@
+{
+    //  lenght
+    const readline = require('readline');
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    
+    rl.question("Masukkan sebuah kalimat: ", (inputString) => {
+        // Contoh property lenght
+        console.log(`Panjang kalimat: ${inputString.length}`);
+        rl.close();
+    });
+    }
+
+    "Panjang Kalimatnya adalah: 33"
